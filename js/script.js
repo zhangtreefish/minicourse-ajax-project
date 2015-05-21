01e93d9f6b1d38a37c83859e827c3a9d7b93d240
zhangtreefish <zhangtreefish@users.noreply.github.com>
2015-05-21 12:38:08 -0500

    fix(script): move retrun false to near end to allow running of nyt API

--- js/script.js
+++ js/script.js
@@ -16,13 +16,28 @@ function loadData() {
     // YOUR CODE GOES HERE!
     var streetVal=$("#street").val();
     var cityVal=$("#city").val();
+    console.log(cityVal);//this works
     var address=streetVal+","+cityVal;
-    var urlVal="https://maps.googleapis.com/maps/api/streetview?size=600x400&location="+address;//the +'' in the key is not necessary
-    //var imageVal=.getAjax(urlVal){
-    //    key: "AIzaSyCyHY2v3Zf07pPIwa3eTJxgf1TWZSfUyC8";
-    //}
+    var urlVal='https://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address;//the +'' in the course answer is not necessary
+
     $body.append('<img class="bgimg" src="' +urlVal+ '">');//why is "' +   +'" required?
-    return false;
-};
+    //return false; //without this line the image flashes and then goes away. But with this the nyt does not run.Why?
+
+    var nyKey='e831cd846809f7725229b8ce66e3436f:15:71398341';
+    var nyAPI='http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityVal+'&sort=newest&api-key='+nyKey;
+    console.log(nyKey);
+    console.log(cityVal);
+    console.log(nyAPI);
+    $.getJSON(nyAPI, function(data){
+        console.log(data);
+        $nytHeaderElem.text('NY Times articles about '+cityVal+':');//need ()
+        articles=data.response.docs;//not "var articles"!
+        for (var i=0; i<3; i++) {
+            var article=articles[i];
+            $nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'</li>');
+        } //for
+    });//.get.JSON
+
+};//loadData
 
 $('#form-container').submit(loadData);
